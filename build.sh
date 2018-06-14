7ac05df76f27c0abe82542b86b622a5c51c33a0e
Mykola Grechukh <mgrechukh@satelliz.com>
2018-06-15 01:31:05 +0300

    label fix

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 image_name=alpine_croutons
 docker build -t $image_name .
 
-docker run --rm $image_name sh -c "rm -f /.dockerenv; tar cfz - -C / -V $image_name --one-file-system ." > out/$image_name.tar.gz
+docker run --rm $image_name sh -c "rm -f /.dockerenv; tar cfj - -C / -V crouton:bootstrap.$image_name --one-file-system ." > out/$image_name.tar.bz2
 docker rmi -f $image_name
