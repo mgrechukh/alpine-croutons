f467c37adf2a2c1fe965d33e2c5a2b5f7617def1
Mykola Grechukh <mgrechukh@satelliz.com>
2018-06-15 01:03:26 +0300

    only gz allowed

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 image_name=alpine_croutons
 docker build -t $image_name .
 
-docker run --rm $image_name sh -c "rm -f /.dockerenv; tar cfj - -C / -V $image_name --one-file-system ." > $image_name.tar.bz2
+docker run --rm $image_name sh -c "rm -f /.dockerenv; tar cfz - -C / -V $image_name --one-file-system ." > out/$image_name.tar.gz
 docker rmi -f $image_name
