5937f5c2e546b02c52b2d132f8578e6ea5049a12
Mykola Grechukh <mgrechukh@satelliz.com>
2018-06-15 17:53:21 +0300

    gz

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ fi
 
 docker build -t $image_name .
 
-docker run --rm $image_name sh -c "rm -f /.dockerenv; tar cfj - -C / -V crouton:bootstrap.$image_name --one-file-system ." > out/$image_name.tar.bz2
+docker run --rm $image_name sh -c "rm -f /.dockerenv; tar cfj - -C / -V crouton:bootstrap.$image_name --one-file-system ." > out/$image_name.tar.gz
 docker rmi -f $image_name
