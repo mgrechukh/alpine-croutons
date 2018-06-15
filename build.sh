3ddd675ff975db11577cf4939aabb543c4b60e79
Mykola Grechukh <mgrechukh@satelliz.com>
2018-06-15 17:41:02 +0300

    allow to specify image name

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-image_name=alpine_croutons
+if [ -z "$1" ]; then
+	image_name=alpine_croutons
+else
+	image_name=$1
+fi
+
 docker build -t $image_name .
 
 docker run --rm $image_name sh -c "rm -f /.dockerenv; tar cfj - -C / -V crouton:bootstrap.$image_name --one-file-system ." > out/$image_name.tar.bz2
